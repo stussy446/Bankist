8a1010edfd4ed6e07f16904d6ed1faa928021831
Steve Rector <steverector27@gmail.com>
2023-09-21 14:49:42 -0700

    Adds smooth scrolling when learn more button clicked

--- starter/script.js
+++ starter/script.js
@@ -7,6 +7,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -29,4 +32,10 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+btnScrollTo.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
